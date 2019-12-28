dbe21f3e6ca4b1db5086058f869ea132eba39bb7
emanayman657 <36210449+emanayman657@users.noreply.github.com>
2019-12-28 20:58:15 +0200

    Add files via upload

---
+++ friends.java
@@ -0,0 +1,66 @@
+package signup;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Vector;
+;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+public class friends  {
+    //public  Vector<signup>name_=new Vector<>();
+/*
+    public friends() {
+        this.name_=new Vector<>();
+
+    }*/
+    public String na;
+    public friends(){
+        this.na="";
+    }
+    public void addfriend(){
+
+        System.out.println("friend request sended");
+
+    }
+    public void acceptfriend(){
+
+        System.out.println("Accept friend request");
+
+    }}
+     /*  Enumeration en=this.name.elements();
+    while (en.hasMoreElements()) {
+            System.out.println(en.nextElement());}}}
+        //  System.out.println(en);
+         /* int  length=users.length;
+                  System.out.println(length);
+                 Scanner in =new  Scanner(System.in);
+                 System.out.println("enter name of friend");
+               String  names=in.next();
+                for (int i=0;i<length;i++){
+
+               if (names.equals(users[i])){
+               arr.add(names);
+
+               }   System.out.println(arr); }
+           int ch;
+           ch=in.nextInt();
+          // for(int k=0;k<arr.size();k++){
+               System.out.println("send friend request to "+arr.get(ch)); */
+
+
+
+
+
+
+
+
+
+
+
